e81b1232e1e593ad9747f11ec612d06310233494
nurajalali <jalalinura@gmail.com>
2023-05-24 17:23:39 +0330

    first version

---
+++ script.js
@@ -0,0 +1,44 @@
+const state = { tip: 0, people: 0, amount: 0 };
+
+const tipSpan = document.getElementById("tipPerPerson");
+const totalSpan = document.getElementById("totalPerPerson");
+tipSpan.innerText = "0.00";
+totalSpan.innerText = "0.00";
+
+document.getElementById("billnum").addEventListener("change", (event) => {
+  state.amount = parseInt(event.target.value);
+});
+
+const btns = Array.from(document.getElementsByClassName("tips__btn"));
+
+btns.forEach((btn) => {
+  if (btn.id != "btn_custom") {
+    btn.addEventListener("click", () => {
+      document.getElementById("btn_custom").classList.remove("disabled");
+      document.getElementById("tips__custom").classList.add("disabled");
+      //   state.tip = parseInt(btn.innerHTML.slice(0, btn.innerHTML.length - 1));
+      state.tip = parseInt(btn.innerText);
+    });
+  }
+});
+
+document.getElementById("peopleNum").addEventListener("change", (event) => {
+  state.people = parseInt(event.target.value);
+
+  if (state.people != 0) {
+    const tip = (state.tip / 100) * state.amount;
+    const tipPerPerson = tip / state.people;
+    const totalPerPerson = (state.amount + state.tip) / state.people;
+    tipSpan.innerText = tipPerPerson.toFixed(2);
+    totalSpan.innerText = totalPerPerson.toFixed(2  );
+  }
+});
+
+document.getElementById("btn_custom").addEventListener("click", (event) => {
+  document.getElementById("tips__custom").classList.remove("disabled");
+  event.currentTarget.classList.add("disabled");
+});
+
+document.getElementById("tips__custom").addEventListener("change", (event) => {
+  state.tip = parseInt(event.target.value);
+});
